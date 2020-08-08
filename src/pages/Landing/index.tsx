a1dbf211192ecaf1919d6df4d234529453619fc3
Lucas Oliveira <hattori_@outlook.com>
2020-08-08 01:49:17 -0300

    Part 2

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -4,7 +4,7 @@ import landingImg from '../../assets/images/landing.svg'
 import study from '../../assets/images/icons/study.svg'
 import giveClasses from '../../assets/images/icons/give-classes.svg'
 import purpleHeart from '../../assets/images/icons/purple-heart.svg'
-
+import { Link } from 'react-router-dom'
 import './styles.css'
 
 function Landing() {
@@ -17,14 +17,14 @@ function Landing() {
             </div>
             <img src={landingImg} alt="landing-img" className="hero-image"/>
             <div className="buttons-container">
-                <a href="/" className="study">
+                <Link to="/study" className="study">
                     <img src={study} alt="estudar-icon"/>
                     Estudar
-                </a>
-                <a href="/" className="give-classes" >
+                </Link>
+                <Link to="/give-classes" className="give-classes" >
                     <img src={giveClasses} alt="dar-aulas-icon"/>
                     Dar Aulas
-                </a>
+                </Link>
             </div>
             <span className="total-connections">
                 Total de 200 conexões ja realizadas <img src={purpleHeart} alt="Coração roxo"/>
