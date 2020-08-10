2b1d06a6ceade603231d44356972b0f3d5127117
Lucas Oliveira <hattori_@outlook.com>
2020-08-10 15:28:27 -0300

    add rules prettier and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,46 +1,54 @@
 module.exports = {
   env: {
     browser: true,
-    es2020: true,
+    es2020: true
   },
   extends: [
     'airbnb',
     'prettier/@typescript-eslint',
     'plugin:react/recommended',
     'plugin:prettier/recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 11,
-    sourceType: 'module',
+    sourceType: 'module'
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
+  plugins: ['react', '@typescript-eslint'],
   rules: {
-    "react/jsx-filename-extension": [
-      1,
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
       {
-        "extensions": [".tsx"]
+        ts: 'never',
+        tsx: 'never'
       }
     ],
-    "import/extensions": [
-      "error",
-      "ignorePackages",
-      {
-        ts: "never",
-        tsx: "never",
-      },
-    ],
-    settings: {
-      "import/resolver": {
-        typescript: {}
-      }
-    }
+    indent: ['error', 2],
+    'space-before-blocks': 'error',
+    'keyword-spacing': ['error', { after: true }],
+    'space-infix-ops': 'error',
+    'no-extra-parens': 'error',
+    'no-extra-boolean-cast': 'error',
+    'no-extra-semi': 'error',
+    'no-else-return': 'error',
+    'no-multi-spaces': 'error',
+    'no-useless-return': 'error',
+    'eol-last': ['error', 'always'],
+    'object-curly-spacing': ['error', 'always'],
+    'brace-style': ['error', '1tbs', { allowSingleLine: true }],
+    'array-bracket-spacing': ['error', 'always', { objectsInArrays: false }],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never']
+  },
+  settings: {
+    'import/resolver': { typescript: {} }
   }
-};
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
-import './assets/styles/global.css';
+import React from 'react'
+import './assets/styles/global.css'
 import Landing from './pages/Landing'
 
-function App() {
-  return (
-    <div className="App">
-      <Landing />
-    </div>
-  );
-}
+const App = () => (
+  <div className='App'>
+    <Landing />
+  </div>
+)
 
-export default App;
+export default App
